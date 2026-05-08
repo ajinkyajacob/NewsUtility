dfebf9f8d00c8cc7c4656f95bf1c0bd101af0306
ajinkyajacob <82514259+ajinkyajacob@users.noreply.github.com>
2026-05-08 18:53:48 +0530

    updated the params set logic

--- src/app/interceptors/news-api-token-interceptor.interceptor.ts
+++ src/app/interceptors/news-api-token-interceptor.interceptor.ts
@@ -7,7 +7,6 @@ export const newsApiTokenInterceptorInterceptor: HttpInterceptorFn = (
   next,
 ) => {
   const token = inject(NewsApiToken);
-  const clonedReq = req.clone({ headers: req.headers.set('X-Api-Key', token) });
-  clonedReq.params.set('key', token);
+  const clonedReq = req.clone({ headers: req.headers.set('X-Api-Key', token), params: req.params.set('key', token)});
   return next(clonedReq);
 };
