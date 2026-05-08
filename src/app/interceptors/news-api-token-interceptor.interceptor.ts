0800166365c1691a5230a539bc91c58d80c9068c
ajinkyajacob <ajinkya.jacob@netcorecloud.com>
2026-05-08 20:09:29 +0530

    fixed issue and removed token embedded in UI

--- src/app/interceptors/news-api-token-interceptor.interceptor.ts
+++ src/app/interceptors/news-api-token-interceptor.interceptor.ts
@@ -1,6 +1,4 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
-import { NewsApiToken } from '../news-utility.token';
+import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 
@@ -8,18 +6,20 @@ export const newsApiTokenInterceptorInterceptor: HttpInterceptorFn = (
   req,
   next,
 ) => {
-
   return next(req).pipe(
     catchError((error) => {
-
       console.error('API Error:', error);
 
-      return of({
-        status: 'fail',
-        totalResults: 0,
-        articles: [],
-      });
-
-    })
+      return of(
+        new HttpResponse({
+          status: 200,
+          body: {
+            status: 'fail',
+            totalResults: 0,
+            articles: [],
+          },
+        }),
+      );
+    }),
   );
 };
