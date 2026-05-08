4cf5c0d20fc2b613107013d84c3f9a1550ee0ea2
ajinkyajacob <82514259+ajinkyajacob@users.noreply.github.com>
2026-05-08 20:02:08 +0530

    removed front end api call

--- src/app/interceptors/news-api-token-interceptor.interceptor.ts
+++ src/app/interceptors/news-api-token-interceptor.interceptor.ts
@@ -1,26 +1,25 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { NewsApiToken } from '../news-utility.token';
-import { catchError} from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 
 export const newsApiTokenInterceptorInterceptor: HttpInterceptorFn = (
   req,
   next,
 ) => {
-  const token = inject(NewsApiToken);
-  const clonedReq = req.clone({
-    //  headers: req.headers.set('X-Api-Key', token),
-     params: req.params.set('apiKey', token)
-    });
+
   return next(req).pipe(
     catchError((error) => {
+
       console.error('API Error:', error);
+
       return of({
         status: 'fail',
         totalResults: 0,
         articles: [],
       });
+
     })
   );
 };
