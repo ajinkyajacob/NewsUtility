d64f51b4016cfa14d2de7c514e4d544f8dfba675
ajinkyajacob <82514259+ajinkyajacob@users.noreply.github.com>
2026-05-08 19:58:44 +0530

    of observable fn was not imported

--- src/app/interceptors/news-api-token-interceptor.interceptor.ts
+++ src/app/interceptors/news-api-token-interceptor.interceptor.ts
@@ -2,6 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { NewsApiToken } from '../news-utility.token';
 import { catchError} from 'rxjs/operators';
+import { of } from 'rxjs';
 
 export const newsApiTokenInterceptorInterceptor: HttpInterceptorFn = (
   req,
